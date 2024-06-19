d3d3c308b1557ddd95e035a08562cf77912ef834
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-06-19 22:30:37 +0800

    Update pca.py

--- AI_ML_training/pca.py
+++ AI_ML_training/pca.py
@@ -10,27 +10,21 @@ class PCA:
         self.components = None
 
     def fit(self, X: np.ndarray) -> None:
-
-        self.mean = np.mean(X,axis=0)
-        self.std = np.std(X,axis=0)
-        X_std = (X - self.mean)/self.std
-        cov_matrix = np.cov(X_std.T)
-        eigen_values, eigen_vectors = np.linalg.eig(cov_matrix)
-        max_idx = np.argmax(np.abs(eigen_vectors),axis=0)
-        feature_signs = np.signs(eigen_vectors[max_idx,range(eigen_vectors.shape[0])])
-        eigen_vectors *= feature_signs[np.newaxis,:]
-        eigen_vectors = eigen_vectors.T
-        eigen_pairs = [(np.abs(eigen_values[i]),eigen_vectors[i,:]) for i in range(len(eigen_values))]
-        eigen_pairs.sort(key=lambda x: x[0], reverse=True)
-        eigen_values_sorted = np.array([x[0] for x in eigen_pairs])
-        eigen_vectors_sorted = np.array([x[1] for x in eigen_pairs])
+        #TODO: 10%
         
-        self.components = eigen_vectors_sorted[:self.n_components,:]
-
+        X_norm = self.normalize(X)
+        cov_matrix = np.cov(X_norm.T)
+        eigen_values, eigen_vectors = np.linalg.eigh(cov_matrix)
+        sorted_idx = np.argsort(np.abs(eigen_values))[::-1]
+        eigen_vectors_sorted = eigen_vectors[:, sorted_idx]
+        
+        self.components = eigen_vectors_sorted[:, :self.n_components].T
+    
+    def normalize(self,X: np.ndarray) -> np.ndarray:
+        return X - np.mean(X, axis=0)
+        
     def transform(self, X: np.ndarray) -> np.ndarray:
+        return np.dot(self.normalize(X), self.components.T)
 
-        raise NotImplementedError
-
-    def reconstruct(self, X):
-        raise NotImplementedError
-
+    def reconstruct(self, X: np.ndarray) -> np.ndarray:
+        return np.dot(self.transform(X), self.components) + np.mean(X, axis=0)
