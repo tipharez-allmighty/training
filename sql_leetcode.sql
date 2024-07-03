65546cf97c7f2d60bb709b463846d7fbe954dd03
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-07-03 18:12:07 +0800

    Update sql_leetcode.sql

--- sql_leetcode.sql
+++ sql_leetcode.sql
@@ -107,3 +107,24 @@ CROSS JOIN Subjects AS sb
 LEFT JOIN Examinations AS e ON s.student_id = e.student_id AND e.subject_name = sb.subject_name
 GROUP BY s.student_id, s.student_name, sb.subject_name
 ORDER BY s.student_id ASC, s.student_name ASC;
+
+/*
+620. Not Boring Movies
+*/
+SELECT *
+FROM Cinema
+WHERE description != 'boring' And id % 2 = 1
+ORDER BY rating DESC;
+
+/*
+1251. Average Selling Price
+*/
+SELECT product_id, ROUND(CASE WHEN total_units = 0 THEN 0 ELSE total_price / total_units END, 2) AS average_price
+FROM (
+    SELECT p.product_id, SUM(p.price*s.units) AS total_price, COALESCE(SUM(s.units), 0) AS total_units
+    FROM Prices AS p
+    LEFT JOIN UnitsSold AS s 
+    ON p.product_id = s.product_id
+    AND s.purchase_date BETWEEN p.start_date AND p.end_date
+    GROUP BY p.product_id
+) AS subquery;
