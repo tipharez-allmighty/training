d921eb8a2875c74a62fbb690325901bf0c148f25
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-09-03 02:07:00 +0800

    Create leetcode.c

---
+++ leetcode.c
@@ -0,0 +1,20 @@
+// binary search
+int search(int* nums, int numsSize, int target) {
+    int low = 0;
+    int high = numsSize - 1;
+    
+    while (high>=low){
+        int mid = low + (high - low)/2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+        if (nums[mid] > target) {
+            high = mid - 1;
+        }
+        if (nums[mid] < target) {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
