8d1b96e24ccdb1f061e114c921093925361fc312
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-09-03 01:55:46 +0800

    Create leetcode.js

---
+++ leetcode.js
@@ -0,0 +1,18 @@
+// binary search
+var search = function(nums, target) {
+    low = 0;
+    high = nums.length - 1;
+    while (high>=low){
+        mid = low + Math.floor((high - low)/2);
+        if (nums[mid] == target){
+            return mid;
+        }
+        if (target > nums[mid]){
+            low = mid + 1;
+        }
+        if (target < nums[mid]){
+            high = mid - 1
+        }
+    }
+    return -1
+};
