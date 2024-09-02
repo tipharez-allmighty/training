6570de95462057f211a45646f4b5a5a43a8d9a34
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-09-03 02:49:45 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,4 +1,4 @@
-#binary search
+# binary search
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         low = 0
@@ -13,3 +13,16 @@ class Solution:
             if target < nums[mid]:
                 high = mid - 1
         return -1
+
+# search insert
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while high >= low:
+            mid = low + (high - low)//2
+            if nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return low
