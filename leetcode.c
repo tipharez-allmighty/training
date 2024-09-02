08fbebf7a4e1d5aa6d8e0fc425281b481385e00d
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-09-03 03:04:58 +0800

    Update leetcode.c

--- leetcode.c
+++ leetcode.c
@@ -18,3 +18,20 @@ int search(int* nums, int numsSize, int target) {
     }
     return -1;
 }
+
+// search insert
+int searchInsert(int* nums, int numsSize, int target) {
+    int low = 0;
+    int high = numsSize - 1;
+
+    while (high >= low) {
+        int mid = low + (high - low)/2;
+
+        if (nums[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
