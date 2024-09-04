3d3b2479fd3b74e29cf0ea4b40ba65a4a2b1d04f
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-09-04 19:33:38 +0800

    Update leetcode.c

--- leetcode.c
+++ leetcode.c
@@ -35,3 +35,35 @@ int searchInsert(int* nums, int numsSize, int target) {
     }
     return low;
 }
+
+//Search in Rotated Sorted Array
+int search(int* nums, int numsSize, int target) {
+  int low = 0;
+  int high = numsSize - 1;
+
+  while (low <= high) {
+    int mid = low + (high - low)/2;
+    
+    if (nums[mid] == target) {
+        return mid;
+    }
+    if (nums[mid] >= nums[low]) {
+        if (target < nums[low] || target > nums[mid]) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    else {
+        if (target > nums[high] || target < nums[mid]) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+  }
+  return -1;  
+}
