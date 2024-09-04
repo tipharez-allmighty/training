dcaef8ee41c59eae6d59b29ec717d13fb4e958eb
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-09-04 19:02:22 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -26,3 +26,27 @@ class Solution:
             else:
                 high = mid - 1
         return low
+
+#Search in Rotated Sorted Array
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            if target == nums[mid]:
+                return mid
+            if nums[low] <= nums[mid]:
+                if target < nums[low] or target > nums[mid]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            else:
+                if target > nums[high] or target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+
+        return -1
