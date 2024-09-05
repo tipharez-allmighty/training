3dec52d8e3a03405aa6760be5ddbf24e8bb9ac08
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-09-05 20:16:29 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -62,7 +62,7 @@ def bubbleSort(arr):
 
 # insert sort
 def insertSort(arr):
-    for i in range(len(arr) - 1):
+    for i in range(len(arr)):
         j = i
         while arr[j - 1] > arr[j] and j > 0:
             arr[j], arr[j - 1] = arr[j - 1], arr[j]
