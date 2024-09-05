503e2d60e65876411c0558b634f7eb0be39d1c7f
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-09-05 11:59:15 +0800

    Update leetcode.c

--- leetcode.c
+++ leetcode.c
@@ -67,3 +67,16 @@ int search(int* nums, int numsSize, int target) {
   }
   return -1;  
 }
+
+// bubble sort
+int bubble_sort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
