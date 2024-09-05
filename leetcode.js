1a8c85e8279e989c52ce9e31c3cc07245c065c46
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-09-05 20:06:12 +0800

    Update leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -76,3 +76,15 @@ var bubble_sort = function(nums) {
         }
     }
 }
+
+// insert sort
+var insert_sort = function(nums) {
+    for (i = 0; i < nums.length; i++) {
+        j = i;
+
+        while (j > 0 && nums[j - 1] > nums[j]) {
+            [nums[j - 1], nums[j]] = [nums[j], nums[j - 1]];
+            j--;
+        }
+    }
+}
