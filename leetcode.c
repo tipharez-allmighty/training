fad209d6041bf70fb33cb45c38b2df3ac7e429c1
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-09-21 18:31:41 +0800

    Update leetcode.c

--- leetcode.c
+++ leetcode.c
@@ -96,3 +96,47 @@ void insert_sort(int arr[], int size) {
         }
     }
 }
+
+// heapsort
+void swap(int arr[], int i, int j);
+
+void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+};
+
+void build_heap(int arr[], int size, int i);
+
+void build_heap(int arr[], int size, int i) {
+    int largest = i;
+    int right = 2*i + 1;
+    int left = 2*i + 2;
+    
+    if (left < size && arr[left] > arr[largest]) {
+        largest = left;
+    };
+
+    if (right < size && arr[right] > arr[largest]) {
+        largest = right;
+    };
+
+    if (largest != i) {
+        swap(arr, largest, i);
+        build_heap(arr, size, largest);
+    };
+    
+};
+
+void heapsort(int arr[], int size);
+
+void heapsort(int arr[], int size) {
+    for (int i = size/2 - 1; i >= 0; i--) {
+        build_heap(arr, size, i);
+    };
+
+    for (int i = size - 1; i > 0; i--) {
+        swap(arr, i, 0);
+        build_heap(arr, i, 0);
+    };
+};
