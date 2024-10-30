ef684fce6ca99046c90f512e89ff418115535f96
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-10-31 00:55:35 +0800

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -14,6 +14,22 @@ class Solution:
                 high = mid - 1
         return -1
 
+# Valid Palindrome
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if not s[left].isalnum():
+                left+=1
+            elif not s[right].isalnum():
+                right-=1
+            elif s[left].lower() == s[right].lower():
+                left+=1
+                right-=1
+            else: return False
+        return True
+        
 # search insert
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
