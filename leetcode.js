2f9d1f36990aa485c6bf605085f9fb2a633c7fbf
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2024-11-01 03:55:46 +0800

    Update leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -238,5 +238,25 @@ var quicksort = function(arr, left=0, right=null) {
         let pivot = partition(arr, left, right);
         quicksort(arr, left, pivot - 1);
         quicksort(arr, pivot + 1, right);
+
+// maximum Depth of Binary Tree (recursive)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) {
+        return 0
+    }
+    return 1 + Math.max(maxDepth(root.left), (maxDepth(root.right)))
+};
     }
 }
