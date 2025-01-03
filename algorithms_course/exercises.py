db2b9bffc8beceeeb0fd39cceda479687df9a8a0
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2025-01-04 01:58:29 +0800

    Update exercises.py

--- algorithms_course/exercises.py
+++ algorithms_course/exercises.py
@@ -238,3 +238,14 @@ def isPalindrome(self, head):
         right = right.next
     
     return True
+
+# Middle of the Linked List
+def middleNode(self, head):
+    fast = head
+    slow = head
+    
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+    
+    return slow
