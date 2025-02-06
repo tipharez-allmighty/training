0e3b4d38f361bb1313c88ee4fb49f28b571720e2
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2025-02-07 03:19:41 +0800

    Create queue_list.py

---
+++ data_structures/queue_list.py
@@ -0,0 +1,21 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+    
+    def __repr__(self):
+        values = [str(value) for value in self.queue]
+        return ' '.join(values)
+
+    def isEmpty(self):
+        return len(self.queue) == 0
+    
+    def enqueue(self, value):
+        self.queue.append(value)
+    
+    def dequeue(self):
+        if not self.isEmpty():
+            value_to_return = self.queue[0]
+            self.queue.remove(value_to_return)
+            return value_to_return
+        else:
+            raise IndexError('Queue is empty')
