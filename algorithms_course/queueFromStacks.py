207643979292acad413cb827caa5ceaf5c5ed538
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2025-02-12 18:04:01 +0800

    Add files via upload

---
+++ algorithms_course/queueFromStacks.py
@@ -0,0 +1,27 @@
+class StacksQueue:
+    def __init__(self):
+        self.stack_1 = []
+        self.stack_2 = []
+    
+    def __repr__(self):
+        return str(self.stack_1)
+    
+    def isEmpty(self):
+        return not self.stack_1
+    
+    def enqueue(self, value):
+        self.stack_1.append(value)
+    
+    def dequeue(self):
+        if self.isEmpty():
+            raise IndexError('Queue is empty')
+        while self.stack_1:
+            self.stack_2.append(self.stack_1.pop())
+        value_to_return = self.stack_2.pop()
+        while self.stack_2:
+            self.stack_1.append(self.stack_2.pop())
+            
+        return value_to_return
+    
+    def peek(self):
+        return self.stack_1[0] if self.stack_1 else None\
