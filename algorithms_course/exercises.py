5920b3de7f818e377305a6f4c4fdc215f57fd728
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2025-03-22 06:34:29 +0800

    Update exercises.py

--- algorithms_course/exercises.py
+++ algorithms_course/exercises.py
@@ -631,3 +631,19 @@ def inOrderSuccesorOptimal(root, n):
             succesor = current.data
             current = current.left
     return succesor
+
+# Lowest Common Ancestor of a Binary Tree
+# Time Complexity: O(n)
+# Space Complexity: O(h)
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and right:
+            return root        
+        return left or right   
