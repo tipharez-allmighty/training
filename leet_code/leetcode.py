37122ba79de05cea73572d980a5ce98e4baab0a7
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2025-04-02 04:21:37 +0800

    Update leetcode.py

--- leet_code/leetcode.py
+++ leet_code/leetcode.py
@@ -197,7 +197,7 @@ def isBalanced(some_node):
         balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
         height = 1 + max(left[1], right[1])
         return [balanced, height]
-
+    return dfs(some_node)[0]
 # Is subsequence
 # Time Complexity: O(n), Space Complexity: O(1)
 class Solution:
