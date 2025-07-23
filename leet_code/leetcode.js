aa511bb2bcf855746fa555c2279bebab9fa9f9f2
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2025-07-24 02:10:54 +0800

    Update leetcode.js

--- leet_code/leetcode.js
+++ leet_code/leetcode.js
@@ -1,3 +1,18 @@
+// Reverse String
+var reverseString = function(s) {
+    let p1 = 0;
+    let p2 = s.length - 1;
+    
+    while (p1 < p2) {
+        [s[p1], s[p2]] = [s[p2], s[p1]];
+        p1 += 1;
+        p2 -= 1;
+    }
+    return s.join();
+}
+
+
+
 // Invert Binary Tree
 var invertTree = function(root) {
   if (!root) {
