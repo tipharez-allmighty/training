ec93e516d07088f06eb6146158d049b7d3f41828
tipharez-allmighty <144653473+tipharez-allmighty@users.noreply.github.com>
2025-07-24 02:12:04 +0800

    Update leetcode.py

--- leet_code/leetcode.py
+++ leet_code/leetcode.py
@@ -1,3 +1,21 @@
+# Reverse String
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        p1 = 0
+
+        p2 = len(s) - 1
+
+        while p1 < p2:
+            s[p1], s[p2] = s[p2], s[p1]
+            p1 += 1
+            p2 -= 1
+            
 # Kth Largest Element in a Stream
 # Time Complexity: O(logk) because we dont store all elements in the array, we store only k amount
 # Space Complexity: O(k)
