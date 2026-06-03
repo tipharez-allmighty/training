e23aac8ba54d4033d6175e4b3432f3b7e4d25302
Artem Kolos <144653473+tipharez-allmighty@users.noreply.github.com>
2026-06-04 00:36:51 +0800

    Update heap.go

--- go_files/heap/heap.go
+++ go_files/heap/heap.go
@@ -1,20 +1,20 @@
 // Package heap implements heap for leetcode tasks to use
 package heap
 
-type Heap struct {
+type MaxHeap struct {
 	container []int
 }
 
-func (h *Heap) Len() int {
+func (h *MaxHeap) Len() int {
 	return len(h.container)
 }
 
-func (h *Heap) Push(element int) {
+func (h *MaxHeap) Push(element int) {
 	h.container = append(h.container, element)
 	h.bubbleUp()
 }
 
-func (h *Heap) Pop() int {
+func (h *MaxHeap) Pop() int {
 	h.swap(0, h.Len()-1)
 	maxValue := h.container[h.Len()-1]
 	h.container = h.container[:h.Len()-1]
@@ -22,7 +22,7 @@ func (h *Heap) Pop() int {
 	return maxValue
 }
 
-func (h *Heap) Heapify() {
+func (h *MaxHeap) Heapify() {
 	n := h.Len()
 	i := n/2 - 1
 	for j := i; j >= 0; j-- {
@@ -30,11 +30,11 @@ func (h *Heap) Heapify() {
 	}
 }
 
-func (h *Heap) swap(i, j int) {
+func (h *MaxHeap) swap(i, j int) {
 	h.container[i], h.container[j] = h.container[j], h.container[i]
 }
 
-func (h *Heap) bubbleDown(n, i int) {
+func (h *MaxHeap) bubbleDown(n, i int) {
 	parent := i
 	left := 2*i + 1
 	right := 2*i + 2
@@ -50,10 +50,69 @@ func (h *Heap) bubbleDown(n, i int) {
 	}
 }
 
-func (h *Heap) bubbleUp() {
+func (h *MaxHeap) bubbleUp() {
 	i := h.Len() - 1
 	parent := (i - 1) / 2
 	for i != 0 && h.container[i] > h.container[parent] {
+		h.swap(i, parent)
+		i = parent
+		parent = (i - 1) / 2
+	}
+}
+
+type MinHeap struct {
+	container []int
+}
+
+func (h *MinHeap) Len() int {
+	return len(h.container)
+}
+
+func (h *MinHeap) Push(element int) {
+	h.container = append(h.container, element)
+	h.bubbleUp()
+}
+
+func (h *MinHeap) Pop() int {
+	h.swap(0, h.Len()-1)
+	maxValue := h.container[h.Len()-1]
+	h.container = h.container[:h.Len()-1]
+	h.bubbleDown(h.Len(), 0)
+	return maxValue
+}
+
+func (h *MinHeap) Heapify() {
+	n := h.Len()
+	i := n/2 - 1
+	for j := i; j >= 0; j-- {
+		h.bubbleDown(n, j)
+	}
+}
+
+func (h *MinHeap) swap(i, j int) {
+	h.container[i], h.container[j] = h.container[j], h.container[i]
+}
+
+func (h *MinHeap) bubbleDown(n, i int) {
+	parent := i
+	left := 2*i + 1
+	right := 2*i + 2
+	if left < n && h.container[parent] > h.container[left] {
+		parent = left
+	}
+	if right < n && h.container[parent] > h.container[right] {
+		parent = right
+	}
+	if parent != i {
+		h.swap(parent, i)
+		h.bubbleDown(n, parent)
+	}
+}
+
+func (h *MinHeap) bubbleUp() {
+	i := h.Len() - 1
+	parent := (i - 1) / 2
+	for i != 0 && h.container[i] < h.container[parent] {
 		h.swap(i, parent)
 		i = parent
 		parent = (i - 1) / 2
