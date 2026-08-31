250a5db6b9c07693c809320a7b3dd37573d85d96
Artem Kolos <144653473+tipharez-allmighty@users.noreply.github.com>
2026-08-31 15:13:12 +0800

    Refactor LRUCache and implement LFUCache

--- go_files/system_design/cache/cache.go
+++ go_files/system_design/cache/cache.go
@@ -16,7 +16,7 @@ type Pair struct {
 	val int
 }
 
-func Constructor(capacity int) LRUCache {
+func ConstructorLRU(capacity int) LRUCache {
 	return LRUCache{cache: make(map[int]*list.Element, capacity), llist: list.New(), capacity: capacity}
 }
 
@@ -46,5 +46,98 @@ func (this *LRUCache) Put(key int, value int) {
 		this.llist.Remove(element)
 	}
 	element := this.llist.PushBack(Pair{key: key, val: value})
+	this.cache[key] = element
+}
+
+type LFUCache struct {
+	cache    map[int]*list.Element
+	freq     map[int]*list.List
+	minFreq  int
+	capacity int
+}
+
+type PairFreq struct {
+	Pair
+	freq int
+}
+
+func ConstructorLFU(capacity int) LFUCache {
+	return LFUCache{cache: make(map[int]*list.Element, capacity), freq: make(map[int]*list.List), capacity: capacity}
+}
+
+func (this *LFUCache) Get(key int) int {
+	element, ok := this.cache[key]
+	if !ok {
+		return -1
+	}
+	pairFreq, ok := element.Value.(PairFreq)
+	if !ok {
+		return -1
+	}
+	oldElemList, ok := this.freq[pairFreq.freq]
+	if ok {
+		oldElemList.Remove(element)
+		if oldElemList.Len() == 0 {
+			delete(this.freq, pairFreq.freq)
+			if pairFreq.freq == this.minFreq {
+				this.minFreq++
+			}
+		}
+	}
+	delete(this.cache, key)
+	pairFreq.freq++
+	llist, ok := this.freq[pairFreq.freq]
+	if !ok {
+		llist = list.New()
+		this.freq[pairFreq.freq] = llist
+	}
+	newElement := llist.PushBack(PairFreq{Pair: pairFreq.Pair, freq: pairFreq.freq})
+	this.cache[key] = newElement
+	return pairFreq.val
+}
+
+func (this *LFUCache) Put(key int, value int) {
+	if element, ok := this.cache[key]; ok {
+		oldPair := element.Value.(PairFreq)
+		oldElemList, ok := this.freq[oldPair.freq]
+		if ok {
+			oldElemList.Remove(element)
+			if oldElemList.Len() == 0 {
+				delete(this.freq, oldPair.freq)
+				if oldPair.freq == this.minFreq {
+					this.minFreq++
+				}
+			}
+		}
+		newPair := oldPair
+		newPair.val = value
+		newPair.freq++
+		newElemList, ok := this.freq[newPair.freq]
+		if !ok {
+			newElemList = list.New()
+			this.freq[newPair.freq] = newElemList
+		}
+		element := newElemList.PushBack(PairFreq{Pair: Pair{key: key, val: value}, freq: newPair.freq})
+		this.cache[key] = element
+		return
+	}
+	if len(this.cache) == this.capacity {
+		lfulist := this.freq[this.minFreq]
+		element := lfulist.Front()
+		evictElement := element.Value.(PairFreq)
+		delete(this.cache, evictElement.key)
+		lfulist.Remove(element)
+		if lfulist.Len() == 0 {
+			delete(this.freq, this.minFreq)
+		}
+	}
+	newMinFreq := 1
+	this.minFreq = newMinFreq
+	newElemList, ok := this.freq[newMinFreq]
+	if !ok {
+		newElemList = list.New()
+		this.freq[newMinFreq] = newElemList
+	}
+	element := newElemList.PushBack(PairFreq{Pair: Pair{key: key, val: value}, freq: newMinFreq})
 	this.cache[key] = element
 }
